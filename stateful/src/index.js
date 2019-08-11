00163be2efe64761dd7cdccd5eb636df2b0fdd19
Vishal Patil <vpatil3@cisco.com>
2019-08-11 13:23:11 -0400

    Added stateful app

---
+++ stateful/src/index.js
@@ -0,0 +1,12 @@
+import React, { useState } from 'react';
+import ReactDOM from 'react-dom';
+
+const App = props => {
+  const [counter, setCounter] = useState(0);
+
+  setTimeout(() => setCounter(counter + 1), 1000);
+
+  return <div>{counter}</div>;
+};
+
+ReactDOM.render(<App />, document.getElementById('root'));
