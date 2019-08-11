5b10879a1b19a7c62912b2950aafdbb4fe466386
Vishal Patil <vpatil3@cisco.com>
2019-08-11 17:20:12 -0400

    Able inc/reset counter

--- stateful/src/index.js
+++ stateful/src/index.js
@@ -1,17 +1,30 @@
 import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 
+const Counter = ({ counter }) => {
+  return <div>{counter} </div>;
+};
+
+const Button = ({ onClick, text }) => {
+  return (
+    <div>
+      <button onClick={onClick}> {text} </button>
+    </div>
+  );
+};
+
 const App = props => {
   const [counter, setCounter] = useState(0);
+
   const setValue = value => {
     return () => setCounter(value);
   };
 
   return (
     <div>
-      {counter}
-      <button onClick={setValue(counter + 1)}> Inc </button>
-      <button onClick={setValue(0)}> reset </button>
+      <Counter counter={counter} />
+      <Button onClick={setValue(counter + 1)} text="Inc" />
+      <Button onClick={setValue(0)} text="reset" />
     </div>
   );
 };
