b135444334be545b28d0447c6572df0cdc1d18ad
Vishal Patil <vpatil3@cisco.com>
2019-08-11 21:53:31 -0400

    Able to increment decrement counters

--- left-right-count/src/index.js
+++ left-right-count/src/index.js
@@ -13,7 +13,7 @@ const Counter = ({ left, right }) => {
 const Button = ({ text, onClick }) => {
   return (
     <div>
-      <Button onClick={onClick}> {text} </Button>
+      <button onClick={onClick}> {text} </button>
     </div>
   );
 };
@@ -21,29 +21,25 @@ const Button = ({ text, onClick }) => {
 const App = props => {
   const [clicks, setCounter] = useState({ left: 0, right: 0 });
   const left_click = () => {
-    return () => {
-      const new_clicks = {
-        ...clicks,
-        left: (clicks.left += 1)
-      };
-      setCounter(new_clicks);
+    const new_clicks = {
+      ...clicks,
+      left: (clicks.left += 1)
     };
+    setCounter(new_clicks);
   };
 
   const right_click = () => {
-    return () => {
-      const new_clicks = {
-        ...clicks,
-        right: (clicks.right += 1)
-      };
-      setCounter(new_clicks);
+    const new_clicks = {
+      ...clicks,
+      right: (clicks.right += 1)
     };
+    setCounter(new_clicks);
   };
   return (
     <div>
       <Counter left={clicks.left} right={clicks.right} />
-      <Button onClick={('<=', left_click)}> left </Button>
-      <Button onClick={('=>', right_click)}> right </Button>
+      <Button onClick={left_click} text="left" />
+      <Button onClick={right_click} text="right" />
     </div>
   );
 };
